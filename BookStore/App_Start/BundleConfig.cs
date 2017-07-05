866b0241dc7b4f6a021ed29744f39254501f81fb
natanael lemos <natanael.tavares@ventron.com.br>
2017-07-05 19:10:47 -0300

    Snippets de controllers

--- BookStore/App_Start/BundleConfig.cs
+++ BookStore/App_Start/BundleConfig.cs
@@ -8,15 +8,15 @@ namespace BookStore
         public static void RegisterBundles
             (BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/css/site")
-                .Include("~/Public/css/bootstrap.css")
-                .Include("~/Public/css/app/app.css"));
+            //bundles.Add(new StyleBundle("~/css/site")
+            //    .Include("~/Public/css/bootstrap.css")
+            //    .Include("~/Public/css/app/app.css"));
 
-            bundles.Add(new ScriptBundle("~/js/site")
-                .Include("~/Public/js/jquery-1.10.2.js")
-                .Include("~/Public/js/bootstrap.js")
-                .Include("~/Public/js/jquery.validate.js")
-                .Include("~/Public/js/respond.js"));
+            //bundles.Add(new ScriptBundle("~/js/site")
+            //    .Include("~/Public/js/jquery-1.10.2.js")
+            //    .Include("~/Public/js/bootstrap.js")
+            //    .Include("~/Public/js/jquery.validate.js")
+            //    .Include("~/Public/js/respond.js"));
         }
     }
 }
